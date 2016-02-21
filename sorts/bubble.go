f8a84ce254e88b2d459074458470e2d802570a77
Khari Seals <kseals281@gmail.com>
2016-02-21 05:11:45 -0500

    Improved the swapping in bubble sort

--- sorts/bubble.go
+++ sorts/bubble.go
@@ -8,9 +8,7 @@ func main() {
   for i := 0; i < len(numbers); i++ {
     for j := 0; j < len(numbers) - i - 1; j++ {
       if numbers[j] > numbers[j+1] {
-        temp := numbers[j]
-        numbers[j] = numbers[j+1]
-        numbers[j+1] = temp
+        numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
       }
     }
   }
