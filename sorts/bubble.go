58ecbcb1f59733cd6882776c9530d6158cda26f4
Khari Seals <kseals281@gmail.com>
2016-02-21 04:49:59 -0500

    Adding finished working version of bubble sort

--- sorts/bubble.go
+++ sorts/bubble.go
@@ -1,16 +1,18 @@
 package main
 
 import "fmt"
-import "sort"
 
 func main() {
-  numbers := [5]int{3, 6, 2, 1, 5}
+  numbers := [10]int{2, 1, 5, 2, 3, 7, 6, 8, 4, 0}
+  fmt.Println("Original list: ", numbers)
   for i := 0; i < len(numbers); i++ {
-    for j := 0 + i; j < len(numbers) - 1; j++ {
+    for j := 0; j < len(numbers) - i - 1; j++ {
       if numbers[j] > numbers[j+1] {
-        sort.Swap(numbers[j], numbers[j + 1])
+        temp := numbers[j]
+        numbers[j] = numbers[j+1]
+        numbers[j+1] = temp
       }
     }
   }
-  fmt.Println(numbers)
+  fmt.Println("Sorted list: ", numbers)
 }
